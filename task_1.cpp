d5991f85c2e31e16808ed84b2b90cc2bdb8f7497
Vadim-Ponochevny <Ponovadim67@mail.ru>
2025-04-01 23:11:04 +0700

    task-1 (Linear algorithms)

--- task_1.cpp
+++ task_1.cpp
@@ -2,21 +2,20 @@
 #include <cmath>
 #include <iomanip>
 
-using namespace std; 
+using namespace std;
 
 const double g = 9.81;
-const double PI = 3.141592653589793; 
+const double PI = 3.141592653589793;
 
 int main() {
-    setlocale(LC_ALL, "RU");
 
     double v0, alpha;
-    cout << "Введите начальную скорость (м/с): ";
+    cout << "Enter initial velocity (m/s): ";
     cin >> v0;
-    cout << "Введите угол (в градусах): ";
+    cout << "Enter angle (degrees): ";
     cin >> alpha;
 
-    double alpha_rad = alpha * PI / 180.0; 
+    double alpha_rad = alpha * PI / 180.0;
 
     double flightTime = (2.0 * v0 * sin(alpha_rad)) / g;
     double H = (v0 * v0 * sin(alpha_rad) * sin(alpha_rad)) / (2.0 * g);
@@ -24,13 +23,12 @@ int main() {
     double R1 = (v0 * v0) / (g * cos(alpha_rad));
     double R2 = (v0 * v0 * cos(alpha_rad) * cos(alpha_rad)) / g;
 
-
     cout << fixed << setprecision(6);
-    cout << "Время полета: " << flightTime << " сек" << endl;
-    cout << "Максимальная высота: " << H << " м" << endl;
-    cout << "Максимальная дальность: " << L << " м" << endl;
-    cout << "Радиус кривизны начала траектории: " << R1 << " м" << endl;
-    cout << "Радиус кривизны вершины траектории: " << R2 << " м" << endl;
+    cout << "Flight time: " << flightTime << " sec" << endl;
+    cout << "Maximum height: " << H << " m" << endl;
+    cout << "Maximum range: " << L << " m" << endl;
+    cout << "Radius of curvature at the start of the trajectory: " << R1 << " m" << endl;
+    cout << "Radius of curvature at the peak of the trajectory: " << R2 << " m" << endl;
 
     return 0;
 }
