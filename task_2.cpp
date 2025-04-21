c38181180f54780eec32526dc448d9836c74642e
Vadim-Ponochevny <Ponovadim67@mail.ru>
2025-04-21 22:28:40 +0700

    Update Cycles with pre- and postcondition

--- task_2.cpp
+++ task_2.cpp
@@ -1,32 +1,60 @@
+/***********************************************
+* Author: Ponochevny Vadim                     *
+* Program: Cycles with pre- and postcondition  *
+* Variant: 3                                   *
+***********************************************/
 #include <iostream>
 #include <cmath>
 #include <iomanip>
 
 using namespace std;
 
+double powerCalculation(double resistance, double electromotiveForce, double internalResistance) {
+    if (resistance + internalResistance == 0.0) {
+        return 0.0;
+    }
+    else
+        return resistance * (electromotiveForce / (resistance + internalResistance)) * (electromotiveForce / (resistance + internalResistance));
+}
 
 int main() {
-    const double electromotiveForce = 115.0;
-    const double internalResistance = 29.7;
-    double resistance = 10;
+    double electromotiveForce;
+    double internalResistance;
+    double resistance;
     double currentPower;
-    int marginSize = 2;
+    double firstResistanceLimit = 50.0;
+    double finalResistanceLimit = 300.0;
+    double resistanceStep = 10;
+    int decimalPlaces = 2;
 
-    cout << fixed << setprecision(marginSize)
-         << "R (Ohm)\tP (Watt)\n"
-         << "----------------\n";
-    
-    while (resistance < 50) {
-        currentPower = resistance * pow(electromotiveForce / (resistance + internalResistance), 2);
+    cout << "Load power calculation\n";
+    cout << "----------------------\n";
+
+    cout << "Enter EMF (V): ";
+    cin >> electromotiveForce;
+
+    cout << "Enter internal resistance (Ohm): ";
+    cin >> internalResistance;
+
+    cout << "Enter initial load resistance (Ohm): ";
+    cin >> resistance;
+
+    cout << fixed << setprecision(decimalPlaces)
+        << "R (Ohm)\tP (Watt)\n"
+        << "----------------\n";
+
+    while (resistance < firstResistanceLimit) {
+        currentPower = powerCalculation(resistance, electromotiveForce, internalResistance);
         cout << resistance << "\t" << currentPower << endl;
-        resistance += 10; 
+        resistance += resistanceStep;
     }
-    
+
+    resistanceStep = 50;
     do {
-        currentPower = resistance * pow(electromotiveForce / (resistance + internalResistance), 2);
+        currentPower = powerCalculation(resistance, electromotiveForce, internalResistance);
         cout << resistance << "\t" << currentPower << endl;
-        resistance += 50; 
-    } while (resistance <= 300);
+        resistance += resistanceStep;
+    } while (resistance <= finalResistanceLimit);
 
     return 0;
 }
